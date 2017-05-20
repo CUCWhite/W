056ec271288e89c11890e7d63651e5e560fb592d
wxl <274305314@qq.com>
2017-05-20 17:14:47 +0800

    新的素材

---
+++ game3.02/Assets/script/kutreeburn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kutreeburn : MonoBehaviour {
+
+    bool isBurn;          //枯树是否已经被烧掉
+
+	// Use this for initialization
+	void Start () {
+        isBurn = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "fireball")
+        {
+            isBurn = true;
+            Destroy(this.gameObject, 0.5f);
+        }
+    }
+}
